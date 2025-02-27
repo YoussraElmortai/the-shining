6bf8c95f1b43d80af1ccbdf98e1a7a242eb7079f
Youssra Elmortai <112857270+YoussraElmortai@users.noreply.github.com>
2025-02-27 15:06:46 +0100

    👎

--- main.js
+++ main.js
@@ -1,4 +1,81 @@
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
+//   gsap.set("nav", { y: -150 });
+
+//   const digit1 = document.querySelector(".digit-1");
+//   const digit2 = document.querySelector(".digit-2");
+//   const digit3 = document.querySelector(".digit-3");
+
+//   const splitTextIntoSpans = (selector) => {
+//     const element = document.querySelector(selector);
+//     if (element) {
+//       const text = element.innerText;
+//       const splitText = text
+//         .split("")
+//         .map((char) => `<span>${char}</span>`)
+//         .join("");
+//       element.innerHTML = splitText;
+//     }
+//   };
+
+//   splitTextIntoSpans("header h1");
+
+//   const animate = (digit, targetNumber, duration, delay = 1) => {
+//     const numHeight = digit.querySelector(".num").clientHeight;
+//     const totalDistance = targetNumber * numHeight;
+
+//     console.log(`Animating ${digit.className}:`, { numHeight, totalDistance });
+
+//     gsap.to(digit, {
+//       y: -totalDistance,
+//       duration: duration,
+//       delay: delay,
+//       ease: "power2.inOut",
+//       onComplete: () => console.log(`Animation complete for ${digit.className}`),
+//     });
+//   };
+
+//   animate(digit1, 2, 4);
+//   animate(digit2, 3, 3);
+//   animate(digit3, 7, 5);
+
+//   gsap.to(".progress_bar",{
+//     width: "30%",
+//     duration: 2,
+//     ease: "power4.inOut",
+//     delay: 7,
+//   });
+
+//   gsap.to(".progress_bar",{
+//     width: "100%",
+//     opacity: 0,
+//     duration: 2,
+//     ease: "power3.out",
+//     delay: 8.5,
+//   });
+
+//   gsap.to(".hero_images > img",{
+//     clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+//     duration: 1.5,
+//     ease:"power4.inOut",
+//     stagger: 0.5,
+//     delay: 9,
+//   });
+
+//   gsap.to(".hero",{
+//     scale: 1.25,
+//     duration: 3,
+//     ease:"power3.inOut",
+//     delay: 10,
+//     onComplete:() =>{
+//       gsap.set(".content", {
+//         display: "flex",
+//       });
+//     },
+//   })
+
+// });
+
+window.onload = () => {
   gsap.set("nav", { y: -150 });
 
   const digit1 = document.querySelector(".digit-1");
@@ -17,7 +94,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  splitTextIntoSpans("nav h1");
+  splitTextIntoSpans("header h1");
 
   const animate = (digit, targetNumber, duration, delay = 1) => {
     const numHeight = digit.querySelector(".num").clientHeight;
@@ -38,14 +115,14 @@ document.addEventListener("DOMContentLoaded", () => {
   animate(digit2, 3, 3);
   animate(digit3, 7, 5);
 
-  gsap.to(".progress_bar",{
+  gsap.to(".progress_bar", {
     width: "30%",
     duration: 2,
     ease: "power4.inOut",
     delay: 7,
   });
 
-  gsap.to(".progress_bar",{
+  gsap.to(".progress_bar", {
     width: "100%",
     opacity: 0,
     duration: 2,
@@ -53,24 +130,23 @@ document.addEventListener("DOMContentLoaded", () => {
     delay: 8.5,
   });
 
-  gsap.to(".hero_images > img",{
+  gsap.to(".hero_images > img", {
     clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
     duration: 1.5,
-    ease:"power4.inOut",
+    ease: "power4.inOut",
     stagger: 0.5,
     delay: 9,
   });
 
-  gsap.to(".hero",{
+  gsap.to(".hero", {
     scale: 1.25,
     duration: 3,
-    ease:"power3.inOut",
+    ease: "power3.inOut",
     delay: 10,
-    onComplete:() =>{
+    onComplete: () => {
       gsap.set(".content", {
         display: "flex",
       });
     },
-  })
-
-});
+  });
+};
